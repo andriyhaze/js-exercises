cda845d3857e552cf970540826fc1f7ee090be10
antonio miranda <antsmiranda@gmail.com>
2020-04-14 00:10:34 +0200

    Add exercises for control flow

---
+++ week-3/A-while-loops/exercise.js
@@ -0,0 +1,22 @@
+/*
+  Log the Apollo 11 countdown, use the message provided as the last output.
+  It starts from 8 till 0!
+*/
+
+// write your code here
+var apolloCountdownMessage = "all engine running... LIFT-OFF!";
+var countdown = 8;
+
+console.log(apolloCountdownMessage);
+
+/* EXPECTED OUTPUT */
+// 8
+// 7
+// 6
+// 5
+// 4
+// 3
+// 2
+// 1
+// 0
+// all engine running... LIFT-OFF!
---
+++ week-3/B-for-loops/exercise.js
@@ -0,0 +1,22 @@
+/*
+  Calculate the exponential of the even numbers from 5 to 20
+  Using a for loop and the helper functions provided.
+*/
+
+// write your code here
+function exponential(number) {
+  return number * number;
+}
+
+function isEven(number) {
+  return number % 2 === 0;
+}
+
+/* EXPECTED OUTPUT */
+// The exponential of 6 is 36
+// The exponential of 8 is 64
+// The exponential of 10 is 100
+// The exponential of 12 is 144
+// The exponential of 14 is 196
+// The exponential of 16 is 256
+// The exponential of 18 is 324
